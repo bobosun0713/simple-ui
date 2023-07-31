44ca82229ace131aa4f630ec452fa642564fd9cf
bobosun0713 <chiabo.sun@gmail.com>
2023-07-31 23:37:25 +0800

    feat(components): [icon] add svg-icon components

---
+++ packages/components/src/icon/svg/danger.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg">
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M12 2.98828L22.2924 21H1.70764L12 2.98828ZM4.29241 19.5H19.7076L12 6.01163L4.29241 19.5Z"
+    ></path>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M11.25 15L11.25 10.5L12.75 10.5L12.75 15L11.25 15Z"></path>
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M11.25 17.25L11.25 15.75L12.75 15.75L12.75 17.25L11.25 17.25Z"
+    ></path>
+  </svg>
+</template>
