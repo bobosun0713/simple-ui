9983c020d1f4e983b2c38ac1fa78e9b9f24d712e
bobosun0713 <chiabo.sun@gmail.com>
2023-08-03 14:51:54 +0800

    feat(tsconfig): modify settings in tsconfig

--- packages/components/vitest.config.ts
+++ packages/components/vitest.config.ts
@@ -1,17 +1,28 @@
 import { fileURLToPath } from "node:url";
+import { mergeConfig } from "vite";
 import { configDefaults, defineConfig } from "vitest/config";
+import viteConfig from "./vite.config";
 
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: "jsdom",
-    exclude: [...configDefaults.exclude, "e2e/*"],
-    root: fileURLToPath(new URL("./", import.meta.url)),
-    transformMode: {
-      web: [/\.[jt]sx$/]
-    },
-    coverage: {
-      provider: "v8"
+import type { InlineConfig } from "vitest";
+import type { UserConfig } from "vite";
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
+
+export default mergeConfig(
+  viteConfig as VitestConfigExport,
+  defineConfig({
+    test: {
+      globals: true,
+      environment: "jsdom",
+      exclude: [...configDefaults.exclude, "e2e/*"],
+      root: fileURLToPath(new URL("./", import.meta.url)),
+      transformMode: {
+        web: [/\.[jt]sx$/]
+      },
+      coverage: {
+        provider: "v8"
+      }
     }
-  }
-});
+  }) as VitestConfigExport
+);
