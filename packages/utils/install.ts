a0eabd9a7b86f123cdf34f0e732301c62a74e710
bobosun0713 <chiabo.sun@gmail.com>
2023-08-15 23:07:22 +0800

    feat(utils): [install] add options parameter

--- packages/utils/install.ts
+++ packages/utils/install.ts
@@ -1,10 +1,17 @@
-import type { App, Plugin } from "vue";
+import type { App, Plugin, Directive } from "vue";
 type SFCWithInstall<T> = T & Plugin;
+type SFCOptions = {
+  provide?: Record<string, any>;
+  directive?: Directive;
+};
 
-export const withInstall = <T>(component: T) => {
+export const withInstall = <T>(component: T, options?: SFCOptions) => {
   (component as SFCWithInstall<T>).install = (app: App) => {
     const name = (component as any).name;
     app.component(name, component as SFCWithInstall<T>);
+
+    if (options?.directive) app.directive(name, options.directive);
+    if (options?.provide) app.provide("$" + name, options.provide);
   };
   return component as SFCWithInstall<T>;
 };
