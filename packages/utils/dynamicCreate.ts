c6349fd4ef02c3773ae0bc2059a0505da71a275c
bobosun0713 <chiabo.sun@gmail.com>
2023-08-29 11:05:22 +0800

    feat(utils): [dynamicCreate] modify return value

--- packages/utils/dynamicCreate.ts
+++ packages/utils/dynamicCreate.ts
@@ -5,7 +5,7 @@ export function dynamicCreate(component: Component, props = {}) {
   const container = document.createElement("div");
   const vNode = h(component, props);
   render(vNode, container);
-  return { vNode, component: container.firstChild };
+  return vNode;
 }
 
 export function destroyDynamicCreate(container: HTMLDivElement) {
