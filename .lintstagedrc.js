eb76fc06a3a45582f08b804e57df4f5ad2564100
bobosun0713 <chiabo.sun@gmail.com>
2023-11-07 23:08:18 +0800

    refactor: update husky pre-push hook

---
+++ .lintstagedrc.js
@@ -0,0 +1,18 @@
+function mapFiles(filenames, { lint = true, ts = false } = {}) {
+  const eslint = "eslint --fix --cache";
+  const typeCheck = "tsc --noEmit -p tsconfig.web.json --composite false";
+  const commands = ["prettier --write"];
+
+  if (filenames.length) {
+    if (lint) commands.unshift(`${eslint} ${filenames.join(" ")}`);
+    if (ts) commands.unshift(typeCheck);
+  }
+
+  return commands.map(cmd => (/^vue-tsc/.test(cmd) || /^eslint/.test(cmd) ? cmd : `${cmd} ${filenames.join(" ")}`));
+}
+
+module.exports = {
+  "*.js": filenames => mapFiles(filenames),
+  "*.{ts,vue}": filenames => mapFiles(filenames, { ts: true }),
+  "*.{json,md}": filenames => mapFiles(filenames, { lint: false })
+};
