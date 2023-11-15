4630d837fd9b747e282c697a9a6b6b66dce28231
bobosun0713 <chiabo.sun@gmail.com>
2023-11-15 21:57:28 +0800

    refactor(components): [tooltip] refactor tooltip logic

--- packages/components/src/tooltip/Tooltip.vue
+++ packages/components/src/tooltip/Tooltip.vue
@@ -1,39 +1,152 @@
 <script lang="ts" setup>
-import { ref, computed } from "vue";
-import type { TooltipProps } from "./types";
+import { ref, computed, useSlots, nextTick, watch } from "vue";
+import { useElementBounding, useWindowSize } from "@vueuse/core";
+import type { TooltipProps, TooltipPlacement, TooltipTrigger } from "./types";
 
 defineOptions({
   name: "STooltip"
 });
 
 const props = withDefaults(defineProps<TooltipProps>(), {
-  placement: "bottom",
-  showArrow: true
+  modelValue: false,
+  content: "Text",
+  offset: 8,
+  trigger: "hover",
+  placement: "bottom"
 });
+const emits = defineEmits(["update:modelValue"]);
+const slots = useSlots();
 
 const isVisible = ref(false);
+const top = ref(0);
+const left = ref(0);
+const tooltipRef = ref<HTMLDivElement | null>(null);
+const tooltipContentRef = ref<HTMLDivElement | null>(null);
+const triedPlacements = new Set();
 
-const classesContent = computed(() => [
-  "su-tooltip__content",
-  `su-tooltip__content--${props.placement}`,
-  !props.showArrow ? "su-tooltip__content--disabled-arrow" : ""
-]);
+const { width: windowWidth, height: windowHeight } = useWindowSize();
 
-function handleMouseToggle() {
-  isVisible.value = !isVisible.value;
+const placementPos = computed(() => `top:${top.value}px;left:${left.value}px;`);
+
+function calcPlacementPos(placement: TooltipPlacement) {
+  const boundingOptions = { windowResize: false, windowScroll: false };
+  const {
+    width: tooltipWidth,
+    height: tooltipHeight,
+    y: tooltipY,
+    x: tooltipX
+  } = useElementBounding(tooltipRef, boundingOptions);
+  const { width: tooltipContentWidth, height: tooltipContentHeight } = useElementBounding(
+    tooltipContentRef,
+    boundingOptions
+  );
+  const commonTopValue = Number(tooltipY.value + (tooltipHeight.value - tooltipContentHeight.value) / 2);
+  const commonLeftValue = Number(tooltipX.value + (tooltipWidth.value - tooltipContentWidth.value) / 2);
+
+  const placementMap = {
+    top() {
+      left.value = commonLeftValue;
+      top.value = Number(tooltipY.value - tooltipContentHeight.value) - Number(props.offset);
+    },
+    right() {
+      left.value = Number(tooltipX.value + tooltipWidth.value) + Number(props.offset);
+      top.value = commonTopValue;
+    },
+    bottom() {
+      left.value = commonLeftValue;
+      top.value = Number(tooltipY.value + tooltipHeight.value) + Number(props.offset);
+    },
+    left() {
+      left.value = Number(tooltipX.value - tooltipContentWidth.value) - Number(props.offset);
+      top.value = commonTopValue;
+    }
+  };
+
+  placementMap[placement]?.();
 }
+function checkTouchEdge() {
+  const tooltipContentWidth = tooltipContentRef.value!.getBoundingClientRect().width;
+
+  const edges = [
+    { name: "top", condition: () => top.value <= 0, opposite: "bottom" },
+    { name: "left", condition: () => left.value <= 0, opposite: "right" },
+    {
+      name: "right",
+      condition: () => left.value + tooltipContentWidth >= windowWidth.value,
+      opposite: "left"
+    },
+    { name: "bottom", condition: () => top.value >= windowHeight.value, opposite: "top" }
+  ];
+
+  const touchEdge = edges.find(edge => edge.condition());
+
+  if (touchEdge) {
+    if (triedPlacements.size >= 2) {
+      calcPlacementPos(touchEdge.opposite as TooltipPlacement);
+      triedPlacements.clear();
+      return;
+    }
+
+    calcPlacementPos(touchEdge.opposite as TooltipPlacement);
+    triedPlacements.add(touchEdge.opposite);
+
+    nextTick(() => {
+      checkTouchEdge();
+    });
+  }
+}
+function handleCalcPlacementPos() {
+  nextTick(() => {
+    calcPlacementPos(props.placement);
+    checkTouchEdge();
+  });
+}
+function handleToggle(trigger: TooltipTrigger, event: TooltipTrigger) {
+  if (trigger === event) {
+    isVisible.value = !isVisible.value;
+    emits("update:modelValue", isVisible.value);
+  }
+
+  if (!isVisible.value) return;
+  handleCalcPlacementPos();
+}
+
+watch(
+  () => props.modelValue,
+  () => {
+    isVisible.value = props.modelValue;
+    handleCalcPlacementPos();
+  },
+  { immediate: true }
+);
+watch(
+  () => windowWidth.value,
+  () => handleCalcPlacementPos()
+);
 </script>
 
 <template>
-  <div class="su-tooltip">
-    <div class="su-tooltip__trigger" @mouseenter="handleMouseToggle" @mouseleave="handleMouseToggle">
+  <div ref="tooltipRef" class="su-tooltip">
+    <div
+      class="su-tooltip__trigger"
+      @click="handleToggle(trigger, 'click')"
+      @mouseenter="handleToggle(trigger, 'hover')"
+      @mouseleave="handleToggle(trigger, 'hover')"
+    >
       <slot name="default"></slot>
     </div>
-    <transition name="fade">
-      <div v-show="isVisible && (content || $slots.content)" :class="classesContent">
-        <slot name="content">{{ content }}</slot>
-      </div>
-    </transition>
+    <Teleport to="body">
+      <transition name="fade">
+        <div v-show="isVisible" ref="tooltipContentRef" :style="placementPos" class="su-tooltip__content">
+          <template v-if="slots.content">
+            <slot name="content"></slot>
+          </template>
+          <template v-else>
+            {{ content }}
+          </template>
+        </div>
+      </transition>
+    </Teleport>
   </div>
 </template>
 
--- packages/components/src/tooltip/types.ts
+++ packages/components/src/tooltip/types.ts
@@ -1,7 +1,9 @@
 export type TooltipPlacement = "top" | "bottom" | "left" | "right";
-
+export type TooltipTrigger = "hover" | "click";
 export interface TooltipProps {
+  modelValue?: boolean;
   content?: string | number;
+  offset?: string | number;
+  trigger?: TooltipTrigger;
   placement?: TooltipPlacement;
-  showArrow?: boolean;
 }
