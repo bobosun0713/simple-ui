48f3ad435a4310d34bde28cee5877b3d251436a6
bobosun0713 <chiabo.sun@gmail.com>
2023-11-25 23:51:06 +0800

    fix(typings): modify $loading properties type

--- packages/components/src/loading/Loading.ts
+++ packages/components/src/loading/Loading.ts
@@ -1,11 +1,26 @@
 import { dynamicCreate } from "@simple/utils";
+import { h, ref, onMounted, onUnmounted, createApp, type Ref } from "vue";
+
 import Loading from "./Loading.vue";
 import type { LoadingProps } from "./types";
 
 export default (options?: LoadingProps) => {
+  const a = ref(12);
+  const b = createApp(
+    h(Loading, {
+      spinner: a.value
+    })
+  );
+
+  setTimeout(() => {
+    a.value = 11111;
+  }, 2000);
+
   const vNode = dynamicCreate(Loading, options);
   const vm = vNode.component!;
   document.body.appendChild(vNode.el as HTMLElement);
+
+  console.log("b -> ", b);
 
   function show() {
     vm.exposed!.visible.value = true;
