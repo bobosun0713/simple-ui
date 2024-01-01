de3ce9b1f3c73d6a1eb87b052425c7617a875b88
bobosun0713 <chiabo.sun@gmail.com>
2024-01-01 14:21:13 +0800

    chore: remove defineModel setting

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -2,11 +2,5 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [
-    vue({
-      script: {
-        defineModel: true
-      }
-    })
-  ]
+  plugins: [vue()]
 });
