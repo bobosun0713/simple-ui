0f4829fc62ef419ce3824eecc3ae724e69be975f
bobosun0713 <chiabo.sun@gmail.com>
2024-01-14 21:35:41 +0800

    refactor(components): [grid-item] move to grid folder

--- packages/components/src/grid-item/index.ts
+++
@@ -1,7 +0,0 @@
-import { withInstall } from "@simple/utils";
-import GridItem from ".//GridItem.vue";
-
-const SGridItem = withInstall(GridItem);
-
-export { SGridItem };
-export default SGridItem;
--- packages/components/src/grid/index.ts
+++ packages/components/src/grid/index.ts
@@ -1,7 +1,9 @@
 import { withInstall } from "@simple/utils";
 import Grid from "./Grid.vue";
+import GridItem from ".//GridItem.vue";
 
 const SGrid = withInstall(Grid);
+const SGridItem = withInstall(GridItem);
 
-export { SGrid };
-export default SGrid;
+export { SGrid, SGridItem };
+export default { SGrid, GridItem };
