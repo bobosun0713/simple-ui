19a33b943dfcfd3ebac66ce3545bfb67956a4963
bobosun0713 <chiabo.sun@gmail.com>
2024-01-28 19:41:06 +0800

    refactor(components): [tooltip] modify touch egde logic and coding style

--- packages/components/src/tooltip/Tooltip.vue
+++ packages/components/src/tooltip/Tooltip.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ref, computed, useSlots, nextTick, watch } from "vue";
+import { ref, computed, useSlots, nextTick, watch, onMounted, onUnmounted } from "vue";
 import { useElementBounding, useWindowSize } from "@vueuse/core";
 import { tooltipProps } from "./types";
 import type { TooltipPlacement } from "./types";
@@ -16,10 +16,10 @@ const slots = useSlots();
 const isVisible = ref(false);
 const top = ref(0);
 const left = ref(0);
-const tooltipRef = ref<HTMLDivElement | null>(null);
-const tooltipContentRef = ref<HTMLDivElement | null>(null);
+const tooltipRef = ref<HTMLElement | null>(null);
+const tooltipContentRef = ref<HTMLElement | null>(null);
 const tooltipContentWidth = ref(0);
-const triedPlacements = new Set();
+const tooltipContentHeight = ref(0);
 const currentPlacement = ref("");
 
 const { width: windowWidth, height: windowHeight } = useWindowSize();
@@ -40,14 +40,15 @@ function calcPlacementPos(placement: TooltipPlacement): void {
   );
 
   tooltipContentWidth.value = contentWidth.value;
+  tooltipContentHeight.value = contentHeight.value;
 
-  const commonTopValue = Number(tooltipY.value + (tooltipHeight.value - contentHeight.value) / 2);
+  const commonTopValue = Number(tooltipY.value + (tooltipHeight.value - tooltipContentHeight.value) / 2);
   const commonLeftValue = Number(tooltipX.value + (tooltipWidth.value - tooltipContentWidth.value) / 2);
 
   const placementMap = {
     top() {
       left.value = commonLeftValue;
-      top.value = Number(tooltipY.value - contentHeight.value) - Number(props.offset);
+      top.value = Number(tooltipY.value - tooltipContentHeight.value) - Number(props.offset);
     },
     right() {
       left.value = Number(tooltipX.value + tooltipWidth.value) + Number(props.offset);
@@ -66,35 +67,49 @@ function calcPlacementPos(placement: TooltipPlacement): void {
   placementMap[placement]?.();
 }
 function checkTouchEdge(): void {
-  const edges = [
-    { name: "top", condition: () => top.value <= 0, opposite: "bottom" },
-    { name: "left", condition: () => left.value <= 0, opposite: "right" },
-    {
-      name: "right",
-      condition: () => left.value + tooltipContentWidth.value >= windowWidth.value,
-      opposite: "left"
-    },
-    { name: "bottom", condition: () => top.value >= windowHeight.value, opposite: "top" }
-  ];
-
-  const touchEdge = edges.find(edge => edge.condition());
-
-  if (touchEdge) {
-    currentPlacement.value = touchEdge.opposite;
-
-    if (triedPlacements.size >= 2) {
-      calcPlacementPos(touchEdge.opposite as TooltipPlacement);
-      triedPlacements.clear();
-      return;
-    }
-
-    calcPlacementPos(touchEdge.opposite as TooltipPlacement);
-    triedPlacements.add(touchEdge.opposite);
-
-    nextTick(() => {
-      checkTouchEdge();
-    });
+  // Touch the top edge
+  if (top.value <= 0) {
+    currentPlacement.value = "bottom";
+    calcPlacementPos("bottom");
+    return;
   }
+
+  // Touch the left edge
+  if (left.value <= 0) {
+    currentPlacement.value = "right";
+    calcPlacementPos("right");
+    return;
+  }
+
+  // Touch the right edge
+  if (left.value + tooltipContentWidth.value >= windowWidth.value) {
+    currentPlacement.value = "left";
+    calcPlacementPos("left");
+    return;
+  }
+
+  // Touch the bottom edge
+  if (top.value >= windowHeight.value) {
+    currentPlacement.value = "top";
+    calcPlacementPos("top");
+  }
+}
+
+function isScrollContainer(ele: HTMLElement): boolean {
+  const style = window.getComputedStyle(ele);
+  const overflowRegex = /(auto|scroll)/;
+  return overflowRegex.test(style.overflow);
+}
+function collectScroll(ele: HTMLElement): HTMLElement[] {
+  const scrollList: HTMLElement[] = [];
+  let current = ele?.parentElement;
+  while (current) {
+    if (isScrollContainer(current)) {
+      scrollList.push(current);
+    }
+    current = current.parentElement;
+  }
+  return scrollList;
 }
 
 function handleToggle(): void {
@@ -131,6 +146,17 @@ watch(
   () => windowWidth.value,
   () => triggerPlacement()
 );
+
+onMounted(() => {
+  [window, ...collectScroll(tooltipRef.value as HTMLElement)].forEach(ele => {
+    ele.addEventListener("scroll", triggerPlacement);
+  });
+});
+onUnmounted(() => {
+  [window, ...collectScroll(tooltipRef.value as HTMLElement)].forEach(ele => {
+    ele.removeEventListener("scroll", triggerPlacement);
+  });
+});
 </script>
 
 <template>
