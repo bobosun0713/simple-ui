cb329595e3099be82077f662d80c2be17195f9c8
bobosun0713 <chiabo.sun@gmail.com>
2024-02-12 13:36:29 +0800

    feat(use): add useReactive method

---
+++ packages/use/src/useReactive/index.ts
@@ -0,0 +1,11 @@
+import { reactive, type UnwrapNestedRefs } from "vue";
+import { deepClone } from "@simple/utils";
+
+export function useReactive<T extends object>(target: T): UnwrapNestedRefs<T> & { reset: () => void } {
+  const targetClone = deepClone(target);
+  const initialState = reactive(target) as UnwrapNestedRefs<T> & { reset: () => void };
+
+  initialState.reset = () => Object.assign(initialState, targetClone);
+
+  return initialState;
+}
