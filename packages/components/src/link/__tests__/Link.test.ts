3f91af4e03496c6eccd452e242ee451c04d52cf1
bobosun0713 <chiabo.sun@gmail.com>
2024-04-01 21:58:59 +0800

    test(components): [link] add unit test

---
+++ packages/components/src/link/__tests__/Link.test.ts
@@ -0,0 +1,62 @@
+import { shallowMount } from "@vue/test-utils";
+import Link from "../Link.vue";
+import type { LinkType, LinkTarget } from "../types";
+
+describe("Link.vue", () => {
+  it("should render default structure", async () => {
+    const wrapper = shallowMount(Link);
+
+    expect(wrapper.attributes("class")).toContain("su-link su-link--default su-link--underline");
+  });
+
+  describe("when set props", () => {
+    it.each([
+      ["success", "su-link--success"],
+      ["warning", "su-link--warning"],
+      ["danger", "su-link--danger"]
+    ])("should have `su-link--%s` class", (type: LinkType, expected) => {
+      const wrapper = shallowMount(Link, { props: { type } });
+
+      expect(wrapper.classes(expected)).toBeTruthy();
+    });
+
+    it("should have `href` attribute", () => {
+      const wrapper = shallowMount(Link, { props: { href: "javascript:;" } });
+
+      expect(wrapper.attributes("href")).toBe("javascript:;");
+    });
+
+    it("should not have `href` and `target` attributes", () => {
+      const wrapper = shallowMount(Link);
+
+      expect(wrapper.attributes("href")).toBeUndefined();
+      expect(wrapper.attributes("target")).toBeUndefined();
+    });
+
+    it.each(["_blank", "_self", "_parent", "_top"])("should have `%s` target attribute", (target: LinkTarget) => {
+      const wrapper = shallowMount(Link, { props: { href: "javascript:;", target } });
+
+      expect(wrapper.attributes("target")).toBe(target);
+    });
+
+    it("should have `su-link-underline` class", () => {
+      const wrapper = shallowMount(Link, { props: { underline: true } });
+
+      expect(wrapper.classes("su-link--underline")).toBeTruthy();
+    });
+
+    it("should have `su-link-disabled` class", () => {
+      const wrapper = shallowMount(Link, { props: { disabled: true } });
+
+      expect(wrapper.classes("su-link--disabled")).toBeTruthy();
+    });
+  });
+
+  describe("when set slots", () => {
+    it("should show slot content by default", () => {
+      const wrapper = shallowMount(Link, { slots: { default: "Demo" } });
+
+      expect(wrapper.text()).toBe("Demo");
+    });
+  });
+});
