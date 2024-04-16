e383c59038071ca9cee206f317e32ba94c212701
bobosun0713 <chiabo.sun@gmail.com>
2024-04-16 23:32:03 +0800

    chore(vitest): update settings

--- packages/components/vitest.config.ts
+++ packages/components/vitest.config.ts
@@ -3,26 +3,17 @@ import { mergeConfig } from "vite";
 import { configDefaults, defineConfig } from "vitest/config";
 import viteConfig from "./vite.config";
 
-import type { InlineConfig } from "vitest";
-import type { UserConfig } from "vite";
-interface VitestConfigExport extends UserConfig {
-  test: InlineConfig;
-}
-
 export default mergeConfig(
-  viteConfig as VitestConfigExport,
+  viteConfig,
   defineConfig({
     test: {
       globals: true,
       environment: "jsdom",
       exclude: [...configDefaults.exclude, "e2e/*"],
       root: fileURLToPath(new URL("./", import.meta.url)),
-      transformMode: {
-        web: [/\.[jt]sx$/]
-      },
       coverage: {
         provider: "v8"
       }
     }
-  }) as VitestConfigExport
+  })
 );
