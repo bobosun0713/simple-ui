e677399c77d51d41a39d8ba115963d0a0d4ca847
bobosun0713 <chiabo.sun@gmail.com>
2024-06-10 23:09:49 +0800

    feat(utils): [components] add mountInstance

--- packages/utils/components.ts
+++ packages/utils/components.ts
@@ -1,4 +1,26 @@
+import { createApp, type Component, type App } from "vue";
+
 export function callFn<P extends any[], R>(fn?: ((...arg: P) => R) | ((...arg: P) => R)[] | null, ...args: P): void {
   if (Array.isArray(fn)) fn.map(f => f(...args));
   else if (fn) fn(...args);
+}
+
+export function mountInstance(component: Component): {
+  instance: App;
+  unmount: () => void;
+} {
+  const app = createApp(component);
+  const container = document.createElement("div");
+
+  app.mount(container);
+
+  document.body.appendChild(container);
+
+  return {
+    instance: app,
+    unmount(): void {
+      app.unmount();
+      document.body.removeChild(container);
+    }
+  };
 }
