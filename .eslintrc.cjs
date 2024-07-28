918828687ebe6f1ea14c8cb5eb76763b1d3aec0d
bobosun0713 <chiabo.sun@gmail.com>
2024-07-28 13:45:50 +0800

    build: add eslint-patch

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,8 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
-  },
   extends: [
     "eslint:recommended",
     "plugin:vue/vue3-strongly-recommended",
