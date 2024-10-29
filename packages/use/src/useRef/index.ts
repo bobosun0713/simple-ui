a832b3eb1138081cbfee6e645b8b48d6ff81b11d
bobosun0713 <chiabo.sun@gmail.com>
2024-10-29 23:29:37 +0800

    refactor(use): [useRef] adjust type

--- packages/use/src/useRef/index.ts
+++ packages/use/src/useRef/index.ts
@@ -5,14 +5,12 @@ type RefWithReset<T> = Ref<T> & { reset: () => void };
 
 export function useRef<T>(target: T): RefWithReset<T> {
   const targetClone = deepClone(target);
-  const initialState = ref(target) as RefWithReset<T>;
+  const initialState = ref(target) as unknown as RefWithReset<T>;
 
   initialState.reset = (): void => {
-    if (typeof initialState.value === "object" && initialState.value !== null) {
+    if (typeof initialState.value === "object" && initialState.value !== null)
       initialState.value = Object.assign(initialState.value, deepClone(targetClone));
-    } else {
-      initialState.value = targetClone;
-    }
+    else initialState.value = targetClone;
   };
 
   return initialState;
