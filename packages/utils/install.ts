77615d7f39d3a2d3a6273937035cf1dd2126a02c
bobosun0713 <chiabo.sun@gmail.com>
2024-11-02 20:03:23 +0800

    refactor(utils): refactor install method

--- packages/utils/install.ts
+++ packages/utils/install.ts
@@ -1,32 +1,32 @@
 import type { App, AppContext, Plugin, Directive, Component } from "vue";
 
 type SFCWithInstall<T> = T & Plugin;
+
 type SFCInstallWithContext<T> = SFCWithInstall<T> & {
   _context: AppContext | null;
 };
 
-export const withInstall = <T>(component: T & Component): SFCWithInstall<T> => {
-  (component as SFCWithInstall<T>).install = (app: App): void => {
-    const { name } = component;
-    if (name) app.component(name, component);
-  };
+export function withInstall<T extends Component>(component: T): SFCWithInstall<T> {
+  return Object.assign(component, {
+    install(app: App) {
+      if (component.name) app.component(component.name, component);
+    }
+  });
+}
 
-  return component as SFCWithInstall<T>;
-};
+export function withInstallDirective<T extends Directive>(directive: T, name: string): SFCWithInstall<T> {
+  return Object.assign(directive, {
+    install(app: App) {
+      app.directive(name, directive);
+    }
+  });
+}
 
-export const withInstallFunction = <T>(fn: T, name: string): SFCInstallWithContext<T> => {
+export function withInstallFunction<T>(fn: T, name: string): SFCInstallWithContext<T> {
   (fn as SFCWithInstall<T>).install = (app: App): void => {
     (fn as SFCInstallWithContext<T>)._context = app._context;
     app.config.globalProperties[name] = fn;
   };
 
   return fn as SFCInstallWithContext<T>;
-};
-
-export const withInstallDirective = <T extends Directive>(directive: T, name: string): SFCWithInstall<T> => {
-  (directive as SFCWithInstall<T>).install = (app: App): void => {
-    app.directive(name, directive);
-  };
-
-  return directive as SFCWithInstall<T>;
-};
+}
