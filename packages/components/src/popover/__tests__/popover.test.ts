89f12d85cdc329646d75c6af121f2dfaa2060f36
bobosun0713 <chiabo.sun@gmail.com>
2024-12-16 00:17:54 +0800

    test(components): [popover] update test cases

--- packages/components/src/popover/__tests__/popover.test.ts
+++ packages/components/src/popover/__tests__/popover.test.ts
@@ -1,13 +1,12 @@
-import { nextTick } from "vue";
-import { mount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 
 import Popover from "../Popover.vue";
 
-describe("Popover", () => {
+describe("Popover.vue", () => {
   it("should render default structure", () => {
-    const wrapper = mount(Popover, {
+    const wrapper = shallowMount(Popover, {
       slots: {
-        default: `<div>Demo</div>`
+        default: "Demo"
       },
       global: {
         stubs: {
@@ -16,18 +15,19 @@ describe("Popover", () => {
       }
     });
 
-    expect(wrapper.find(".su-tooltip").exists()).toBeTruthy();
-    expect(wrapper.find(".su-tooltip__trigger").exists()).toBeTruthy();
-    expect(wrapper.find(".su-popover__arrow").exists()).toBeTruthy();
-    expect(wrapper.find(".su-tooltip__trigger").text()).toBe("Demo");
+    expect(wrapper.find(".su-popover").exists()).toBeTruthy();
+    expect(wrapper.find(".su-popover").text()).toBe("Demo");
   });
 
-  describe("when set slots", () => {
-    it("should show popover content when set content slot", async () => {
-      const wrapper = mount(Popover, {
+  describe("when set props", () => {
+    it("should show content prop", () => {
+      const wrapper = shallowMount(Popover, {
+        props: {
+          modelValue: true,
+          content: "Popover content"
+        },
         slots: {
-          default: `<div>Demo</div>`,
-          content: `Tooltip slots content`
+          default: "Demo"
         },
         global: {
           stubs: {
@@ -36,11 +36,82 @@ describe("Popover", () => {
         }
       });
 
-      await wrapper.find(".su-tooltip__trigger").trigger("mouseenter");
-      await nextTick();
+      expect(wrapper.find(".su-popover__content").text()).toBe("Popover content");
+    });
 
-      expect(wrapper.find(".su-popover").exists()).toBe(true);
-      expect(wrapper.find(".su-popover__inner").text()).toBe("Tooltip slots content");
+    it("should disabled arrow style", () => {
+      const wrapper = shallowMount(Popover, {
+        props: {
+          modelValue: true,
+          content: "Popover content",
+          hasArrow: false
+        },
+        slots: {
+          default: "Demo"
+        },
+        global: {
+          stubs: {
+            teleport: true
+          }
+        }
+      });
+
+      expect(wrapper.find(".su-popover__arrow").exists()).toBeFalsy();
+    });
+  });
+
+  describe("when set slots", () => {
+    it("should show content slot", () => {
+      const wrapper = shallowMount(Popover, {
+        props: { modelValue: true },
+        slots: {
+          default: "Demo",
+          content: "Popover slots content"
+        },
+        global: {
+          stubs: {
+            teleport: true
+          }
+        }
+      });
+
+      expect(wrapper.find(".su-popover__content").text()).toBe("Popover slots content");
+    });
+  });
+
+  describe("when set trigger", () => {
+    it.each([
+      { props: { trigger: "click" as const }, event: "click", expectedDisplay: "" },
+      { props: { trigger: "hover" as const }, event: "mouseenter", expectedDisplay: "" },
+      { props: { trigger: "hover" as const, modelValue: true }, event: "mouseleave", expectedDisplay: "none" }
+    ])("should trigger $event event", async ({ props, event, expectedDisplay }) => {
+      const wrapper = shallowMount(Popover, {
+        props: { ...props },
+        slots: { default: "Demo" },
+        global: { stubs: { teleport: true } }
+      });
+
+      const contentEl = wrapper.find(".su-popover__content").element as HTMLElement;
+
+      await wrapper.find(".su-popover").trigger(event);
+
+      expect(contentEl.style.display).toBe(expectedDisplay);
+    });
+
+    it("should only allow trigger actions `click` and `hover`", async () => {
+      const wrapper = shallowMount(Popover, {
+        props: { trigger: [] },
+        slots: { default: "Demo" },
+        global: { stubs: { teleport: true } }
+      });
+
+      const contentEl = wrapper.find(".su-popover__content").element as HTMLElement;
+
+      await wrapper.find(".su-popover").trigger("click");
+      expect(contentEl.style.display).toBe("none");
+
+      await wrapper.find(".su-popover").trigger("mouseenter");
+      expect(contentEl.style.display).toBe("none");
     });
   });
 });
